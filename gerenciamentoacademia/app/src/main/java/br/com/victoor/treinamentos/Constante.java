8d0c80ae641f1fd6997b420483a3ff8635f87809
envolvimento <thalesonice@gmail.com>
2025-06-11 13:40:09 -0300

    Add files via upload

---
+++ gerenciamentoacademia/app/src/main/java/br/com/victoor/treinamentos/Constante.java
@@ -0,0 +1,7 @@
+package br.com.thales.treinamentos;
+
+public interface Constante {
+      String FIELD_EMAIL = "email";
+      String COLLECTION_USERS = "users";
+      String FIELD_ADMIN = "admin";
+}
